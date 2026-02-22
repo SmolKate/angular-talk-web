ac56311e713abd5b86d6b9b17f11f3fcabbf8bd6
SmolKate <zdobnovaea@gmail.com>
2026-02-22 22:28:36 +0300

    feat: routing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,25 +1,12 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProfileCard } from "./common-ui/profile-card/profile-card";
-import { ProfileService } from './data/services/profileServise';
-import { IProfile } from './data/interfaces/profileService.interfaces';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProfileCard],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('talk-web');
-  profileService = inject(ProfileService);
-  profiles: IProfile[] | null = null
-
-  constructor() {
-    this.profileService.getTestAccounts()
-      .subscribe(value => {
-        this.profiles = value
-        console.log(this.profiles)
-  });
-  }
 }
---
+++ src/app/common-ui/layout/layout.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from "@angular/router";
+import { SideBar } from "../side-bar/side-bar";
+
+@Component({
+  selector: 'app-layout',
+  imports: [RouterOutlet, SideBar],
+  templateUrl: './layout.html',
+  styleUrl: './layout.scss',
+})
+export class Layout {
+
+}
